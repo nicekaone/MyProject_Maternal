33c9ab604c5969e6b1ad74f4731ad630de2bc5cf
Maternal <2201102931@student.buksu.edu.ph>
2024-10-13 06:30:10 -0700

    added the valid code

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if _name_ == '_main_':
+    app.run(debug=True)
+
+print ("valid backend code")
+
+
